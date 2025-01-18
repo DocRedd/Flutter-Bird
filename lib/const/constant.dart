fdc670ef2107f7332fc5bce737576e19742654f6
DoctorRedd <ben_mcneilly@yahoo.ca>
2025-01-18 00:11:19 -0400

    Fixed it so that it actually runs hahaha

---
+++ lib/const/constant.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const cardBackgroundColor = Colors.white10;
+const primaryColor = Colors.blue;
+const secondaryColor = Colors.white;
+const backgroundColor = Colors.black;
+const selectionColor = Colors.lightBlueAccent;
+
+const defaultPadding = 20.0;
